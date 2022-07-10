c7f14c050885185618b38622ac31169127d0811e
moerayo <moronfolu.motunrayo1@gmail.com>
2022-07-10 21:27:08 +0100

    chore: initial commit :octocat:

---
+++ init.js
@@ -0,0 +1,5 @@
+import { Appwrite } from 'appwrite';
+export const sdk = new Appwrite();
+sdk
+  .setEndpoint('http://localhost/v1') // Replace this with your endpoint
+  .setProject('62cb325d60c59ac70b17'); // Replace this with your ProjectID
